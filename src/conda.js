5dc49892b0289c33857b51ea18a587a616580ff1
Michele Peresano <peresano@mpp.mpg.de>
2024-12-05 09:41:03 +0100

    use workdir

--- src/conda.js
+++ src/conda.js
@@ -13,19 +13,18 @@ const CONDA_CACHE_NUMBER = core.getInput("conda-cache-number");
 const SHOWYOUWORK_SPEC = core.getInput("showyourwork-spec");
 const RUNNER_OS = shell.env["RUNNER_OS"];
 const RUNNER_NAME = shell.env["RUNNER_NAME"];
+const WORK_DIR = shell.env["RUNNER_WORKSPACE"];
 console.log(`RUNNER GROUP: ${shell.env["RUNNER_GROUP"]}`);
 const isSelfHosted = (shell.env["RUNNER_GROUP"] === "self-hosted" || shell.env["RUNNER_GROUP"] === "undefined");
 console.log(`Is this runner a self-hosted runner?: ${isSelfHosted}`);
 console.log(`Working from: ${process.cwd()}`);
 const showYourWorkCondaDir = isSelfHosted
-  ? `${process.cwd()}/showyourwork_conda_installation`
-  : "~/.conda";
+  ? `${WORK_DIR}/showyourwork_conda_installation`
+  : "~/";
 
 const conda_key = `conda-${constants.conda_cache_version}-${RUNNER_OS}-${CONDA_CACHE_NUMBER}`;
 const conda_restoreKeys = [];
-const conda_paths = isSelfHosted
-  ? [`${showYourWorkCondaDir}/.conda`, `${showYourWorkCondaDir}/condarc`, `${showYourWorkCondaDir}/conda_pkgs_dir`]
-  : ["~/.conda", "~/.condarc", "~/conda_pkgs_dir"];
+const conda_paths = [`${showYourWorkCondaDir}/.conda`, `${showYourWorkCondaDir}/condarc`, `${showYourWorkCondaDir}/conda_pkgs_dir`];
 
 // We'll cache the article unless the user set the cache number to `null` (or empty).
 const CACHE_CONDA = !(CONDA_CACHE_NUMBER == null || CONDA_CACHE_NUMBER == "");
@@ -46,7 +45,7 @@ async function setupConda() {
   }
 
   // Conda installation directory
-  const condaInstallDir = isSelfHosted ? `${showYourWorkCondaDir}/.conda` : "~/.conda";
+  const condaInstallDir = `${showYourWorkCondaDir}/.conda`;
 
   // Download and setup conda
   if (!shell.test("-d", condaInstallDir)) {
